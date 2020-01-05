b1eb038ad6a2f57c3d1b10aee9d80c3d4aaa1d06
Gianluca Fiore <gianlucafiore@papersounds.eu>
2020-01-05 14:51:20 +0100

    update copyright year

--- password_generator.py
+++ password_generator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 ###############################################################################
-# Copyright (c) 2010-2017, Gianluca Fiore
+# Copyright (c) 2010-2020, Gianluca Fiore
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
